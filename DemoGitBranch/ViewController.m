16b54d85dd6fb206fd0164b9d6fc6a1289712df5
pabbypp <1025133834@qq.com>
2018-04-04 10:45:49 +0800

    sunshine分支二次提交后切换至master分支，首次提交

--- DemoGitBranch/ViewController.m
+++ DemoGitBranch/ViewController.m
@@ -17,7 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.launchLbl.textColor = [UIColor blackColor];
+   
+    self.launchLbl.font = [UIFont systemFontOfSize:24];
+    [self.launchLbl sizeToFit];
 }
 
 
